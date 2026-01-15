fdd0c5f82f3d4d0106f06bb860b80b645a2d0458
nanxuan.xie <u8016457@anu.edu.au>
2026-01-16 00:00:05 +0800

    f

---
+++ examples/train_akt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# List of dataset names
+# datasets=("assist2012" "assist2017" "bridge2algebra2006" "nips_task34" "ednet")
+datasets=("assist2015" "assist2017" "assist2012" "bridge2algebra2006" "forget_se")
+
+rm -rf ../data/*/*.pkl
+
+for dataset in "${datasets[@]}"; do
+    echo "===== Running dataset: $dataset ====="
+    python wandb_akt_train.py --dataset_name="$dataset"
+    echo "===== Finished dataset: $dataset ====="
+done
