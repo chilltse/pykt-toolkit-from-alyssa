eb5b5e739fd14557ef58b1b8a5ce5fac59007cf6
nanxuan.xie <u8016457@anu.edu.au>
2026-01-15 21:16:30 +0800

    shell

--- examples/train_aktint.sh
+++ examples/train_aktint.sh
@@ -4,9 +4,8 @@
 # datasets=("assist2012" "assist2017" "bridge2algebra2006" "nips_task34" "ednet")
 datasets=("assist2015" "assist2012" "bridge2algebra2006" "forget_se")
 
-# Loop through each dataset and run the command
-for dataset in "${datasets[@]}"
-do
-    # python wandb_aktint_train.py --dataset_name="$dataset" --use_wandb=1 --add_uuid=0 --num_attn_heads=2 &
-    python wandb_aktint_train.py --dataset_name="$dataset" &
+for dataset in "${datasets[@]}"; do
+    echo "===== Running dataset: $dataset ====="
+    python wandb_aktint_train.py --dataset_name="$dataset"
+    echo "===== Finished dataset: $dataset ====="
 done
